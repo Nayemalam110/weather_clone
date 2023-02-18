5aead105d59381f2212fd378a6bb5e9963174a4a
nayemalam110 <nayemalam110@gmail.com>
2023-02-18 10:07:35 +0600

    alterner Api call

--- lib/service/service.dart
+++ lib/service/service.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:weather/api/api_key.dart';
@@ -18,7 +17,40 @@ class WeatherController extends GetxController {
   }
 
   Future<void> featchWeatherData({lat = 23.811056, lang = 90.407608}) async {
+    feachLocationName(lat, lang);
     final response = await http.get(Uri.parse(apiURL(lat, lang)),
+        headers: {'Accept': 'application/json'});
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      // final user = json.decode(response.body);
+      weathedata.value = WeatherModelFromJson(response.body);
+
+      print(weathedata.value.lat);
+
+      //print(user);
+    } else {
+      Get.defaultDialog(
+          title: 'Erro',
+          content: const Center(
+            child: Text('There is an error'),
+          ),
+          cancel: InkWell(
+              onTap: () => exit(0),
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Close'),
+              )),
+          onCancel: () {
+            Get.back();
+          });
+
+      print('Request failed with status code: ${response.statusCode}');
+    }
+  }
+
+//altNet api
+  Future<void> altFetchWeatherData({lat = 23.811056, lang = 90.407608}) async {
+    final response = await http.get(Uri.parse(altApiURL(lat, lang)),
         headers: {'Accept': 'application/json'});
 
     if (response.statusCode == 200 || response.statusCode == 201) {
@@ -52,6 +84,14 @@ class WeatherController extends GetxController {
     String url;
     url =
         "https://api.openweathermap.org/data/3.0/onecall?lat=$lat&lon=$lon&appid=$apiKey&units=metric&exclude=minutely";
+
+    return url;
+  }
+
+  String altApiURL(lat, lon) {
+    String url;
+    url =
+        "https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$lon&hourly=temperature_2m,surface_pressure,visibility&daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,sunrise,sunset&current_weather=true&timezone=auto";
 
     return url;
   }
